86c94f19b6da534842176615259071d0a7d43665
eytienne <arabeyre.etienne@gmail.com>
2019-08-07 18:03:16 +0200

    webpack config 1

---
+++ src/game.js
@@ -0,0 +1,112 @@
+import '../public/style/game.css'
+import $ from 'jquery'
+
+console.log($);
+
+// const couleur = Math.random();
+// if (couleur < 0.5) {
+// 	const nomPlayerUn = player1.name;
+// 	player1.name = player2.name;
+// 	player2.name = nomPlayerUn;
+// }
+
+// const game = {
+// 	nbCoups: 0,
+// 	board: {
+// 		height: 6,
+// 		width: 7,
+// 	},
+// };
+
+// const tableauDeJeu = [];
+
+// function caseHandler(evt) {
+// 	const winningMove = coup(evt.target.parentNode.getAttribute("col"), evt.target.parentNode.getAttribute("lig"));
+// 	if (winningMove) {
+// 		alert("C'est gagné pour " + (game % 2 == 0 ? player1.name : player2.name));
+// 	}
+// }
+
+// function coup(c, l) {
+// 	console.log("nb coups : " + game.nbCoups);
+// 	c = Number(c);
+// 	l = Number(l);
+// 	const jetonCourant = new Image();
+// 	jetonCourant.src = (game.nbCoups % 2 == 0 ? jetonJaune.src : jetonRouge.src);
+// 	jetonCourant.alt = (game.nbCoups % 2 == 0 ? jetonJaune.alt : jetonRouge.alt);
+// 	tableauDeJeu[l][c].children[0].src = jetonCourant.src;
+// 	tableauDeJeu[l][c].children[0].alt = jetonCourant.alt;
+
+// 	//gestion de la complétion en colonne
+// 	const ret = true;
+// 	const ligneCoulissante = l;
+// 	while (ligneValide(ligneCoulissante - 1) &&
+// 		tableauDeJeu[ligneCoulissante - 1][c].children[0].src == jetonCourant.src) {
+// 		ligneCoulissante--;
+// 	}
+// 	const i = 1;
+// 	for (i = 1; i < 4; ++i) {
+// 		console.log("verif case " + c + " " + (ligneCoulissante + i));
+// 		if (ligneValide(ligneCoulissante + i)) {
+// 			if (ret && tableauDeJeu[ligneCoulissante + i][c].children[0].src != jetonCourant.src)
+// 				ret = false;
+// 		} else
+// 			ret = false;
+// 	}
+// 	if (ret) {
+// 		game.nbCoups++;
+// 		return ret;
+// 	}
+// 	//gestion de la complétion en ligne
+// 	ret = true;
+// 	const colonneCoulissante = c;
+// 	while (colonneValide(colonneCoulissante - 1) &&
+// 		tableauDeJeu[l][colonneCoulissante - 1].children[0].src == jetonCourant.src) {
+// 		colonneCoulissante--;
+// 	}
+// 	for (i = 1; i < 4; ++i) {
+// 		if (colonneValide(colonneCoulissante + i)) {
+// 			console.log("verif case " + (colonneCoulissante + i) + " " + l);
+// 			if (ret && tableauDeJeu[l][colonneCoulissante + i].children[0].src != jetonCourant.src)
+// 				ret = false;
+// 		} else
+// 			ret = false;
+
+// 	}
+// 	if (ret) {
+// 		game.nbCoups++;
+// 		return ret;
+// 	}
+// 	//gestion de la complétion en diagonale croissante
+// 	ret = true;
+// 	colonneCoulissante = c;
+// 	ligneCoulissante = l;
+// 	while (colonneValide(colonneCoulissante - 1) && ligneValide(ligneCoulissante - 1) &&
+// 		tableauDeJeu[l][colonneCoulissante - 1].children[0].src == jetonCourant.src) {
+// 		colonneCoulissante--;
+// 		ligneCoulissante--;
+// 	}
+// 	for (i = 1; i < 4; ++i) {
+// 		if (colonneValide(colonneCoulissante + i) && ligneValide(ligneCoulissante + i)) {
+// 			console.log("verif case " + (colonneCoulissante + i) + " " + (ligneCoulissante + i));
+// 			if (ret && tableauDeJeu[ligneCoulissante + i][colonneCoulissante + i].children[0].src != jetonCourant.src)
+// 				ret = false;
+// 		} else
+// 			ret = false;
+// 	}
+// 	if (ret) {
+// 		game.nbCoups++;
+// 		return ret;
+// 	}
+// 	//gestion de la complétion en diagonale décroissante pour 
+// 	game.nbCoups++;
+// 	return false;
+// }
+
+// function colonneValide(i) {
+// 	return i >= 0 && i < 7;
+// }
+
+// function ligneValide(i) {
+// 	return i >= 0 && i < 6;
+// }
